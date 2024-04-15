4e8b481b8baa9393258eea6c14952a19bb70acde
Bokter <rojasdjuan18@gmail.com>
2024-04-14 19:38:23 -0500

    Calculo de pago terminado, adicion de vehiculos y manejo de estos en pisos

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+import datetime
+import time
+
 class Node:
+
     def __init__(self, data=None, next=None):
         self.data = data
         self.next = next
@@ -24,14 +28,16 @@ class parkingSlots:
             node = node.next
 
 class Piso:
+
     def __init__(self, P):
         self.motos = parkingSlots("G", "L", 20, P)
-        self.carros = parkingSlots("B", "F", 14, P)
+        self.carros = parkingSlots("B", "F", 16, P)
         self.discapacitados = parkingSlots("A", "A", 10, P)
 
         self.plates = []
         self.slots = []
         self.entryTime = []
+        self.type = []
 
         # Lista de vehiculos pq toca hacer 3 listas obligatorias smh
 
@@ -41,16 +47,48 @@ class Piso:
         self.plates.append(vehicle.getPlate())
         self.slots.append(vehicle.getSlot())
         self.entryTime.append(vehicle.getTime())
+        self.type.append(vehicle.getType())
 
-        # Añadir Vehiculo
+        # La misma vaina
 
         self.vehicles.append(vehicle)
 
+    def findVehicle(self, plate): # Se busca el vehiculo en el piso
+        for vehicle in self.vehicles:
+            if vehicle.getPlate() == plate:
+                return vehicle
+        return False
+
+    def removeVehicle(self, plate): # Con la placa eliminas el vehiculo del piso
+        for vehicle in self.vehicles:
+            if vehicle.getPlate() == plate:
+                self.vehicles.remove(vehicle)
+
+    def clearVehicle(self, plate, time):
+        vehicle = self.findVehicle(plate)
+        if vehicle:
+            entry_time = vehicle.getTime().split(":")
+            exit_time = time.split(":")
+            entry_seconds = int(entry_time[0]) * 3600 + int(entry_time[1]) * 60
+            exit_seconds = int(exit_time[0]) * 3600 + int(exit_time[1]) * 60
+            duration = exit_seconds - entry_seconds
+
+            hours_parked = duration / 3600
+            fee = hours_parked * 2000
+            self.removeVehicle(plate)
+            return fee
+        else:
+            return None
+
 class Vehicle:
-    def __init__(self, plate, slot, time):
+
+    # Tiempo del vehiculo evaluado de 00:00 hasta 23:59
+
+    def __init__(self, plate, slot, time, type):
         self.plate = plate
         self.slot = slot
         self.time = time
+        self.type = type
 
     def getPlate(self):
         return self.plate
@@ -61,60 +99,32 @@ class Vehicle:
     def getTime(self):
         return self.time
 
+    def getType(self):
+        return self.type
+
 # Creacion de pisos
 
 Piso_1 = Piso("P1")
 Piso_2 = Piso("P2")
 Piso_3 = Piso("P3")
 
-# Pisos YAY
+# Para la creacion de vehiculos, se necesita PLATE, SLOT, TIME Y TYPE
 
-# print("Piso 1: ")
-# Piso_1.discapacitados.print_list()
-# print("")
-# Piso_1.carros.print_list()
-# print("")
-# Piso_1.motos.print_list()
+# Tipos: Moto, Carro, Discapacitados
 
-def searchVehicle(plate):
-    encontrado = False
-    if len(Piso_1.plates)+len(Piso_2.plates)+len(Piso_3.plates) == 0:
-        return "Las listas estan vacias"
-    else:
-        for i in range(len(Piso_1.plates)):
-            if Piso_1.plates[i] == plate:
-                encontrado = True
-                a = Piso_1.slots[i]
-        for i in range(len(Piso_2.plates)):
-            if Piso_2.plates[i] == plate:
-                encontrado = True
-                a = Piso_2.slots[i]
-        for i in range(len(Piso_3.plates)):
-            if Piso_3.plates[i] == plate:
-                encontrado = True
-                a = Piso_3.slots[i]
 
-    if encontrado:
-       return True
-    else:
-       return False
+vehiculo = Vehicle("ABM650", "P1B1", "10:30", "M")
+Piso_1.addVehicle(vehiculo)
 
-# Por Optimizar
-def addVehicle(plate, slot, time, piso):
+print("Se tiene que pagar: ", Piso_1.clearVehicle("ABM650", "13:30"))
 
-    if searchVehicle(plate) == False: # Si no se encontro el vehiculo, significa que no esta repetido GG WP
-        if piso == 1:
-            Piso_1.addPlates(plate, slot, time)
-        elif piso == 2:
-            Piso_2.addPlates(plate, slot, time)
-        elif piso == 3:
-            Piso_3.addPlates(plate, slot, time)
-    else:
-        return "Vehiculo repetido"
 
-# def montoPago(plate):
 
-    # Lo hago mañana jijijija
+
+
+
+
+
 
 
 
