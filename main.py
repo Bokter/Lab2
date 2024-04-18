862596766c07d5201d3446426df5bfcb57d93565
Bokter <rojasdjuan18@gmail.com>
2024-04-18 14:48:46 -0500

    Todo casi ya hecho, faltan validaciones de carros pero nada mas

--- main.py
+++ main.py
@@ -1,57 +1,83 @@
-import datetime
-import time
-
-class Node:
-
-    def __init__(self, data=None, next=None):
-        self.data = data
-        self.next = next
-
-class parkingSlots:
-
-    def __init__(self, start, end, index, P):
-        self.head = None
-        self.fillSpaces(start, end, index, P)
-
-    def fillSpaces(self, start, end, index, P):
-        for letter in range(ord(end), ord(start) - 1, -1):  # Desde el valor ASCII de end hasta el valor ASCII de start
-            for number in range(index, 0, -1):  # Desde index hasta 1
-                slot = f"{P}{chr(letter)}{number}"
-                newNode = Node(data=slot)
-                newNode.next = self.head
-                self.head = newNode
-
-    def print_list(self):
-        node = self.head
-        while node is not None:
-            print(node.data, end=", ")
-            node = node.next
-
 class Piso:
 
     def __init__(self, P):
-        self.motos = parkingSlots("G", "L", 20, P)
-        self.carros = parkingSlots("B", "F", 16, P)
-        self.discapacitados = parkingSlots("A", "A", 10, P)
+
+        # Listas de vehiculos DISPONIBLES.
+
+        self.motos = []
+        self.carros = []
+        self.discapacitados = []
+
+        self.fillSpaces(P)
 
         self.plates = []
         self.slots = []
         self.entryTime = []
-        self.type = []
+        self.VType = []
 
         # Lista de vehiculos pq toca hacer 3 listas obligatorias smh
 
         self.vehicles = []
 
+    def fillSpaces(self, P):
+
+        # Para motos (de "G1" a "L20", incrementando de 20 en 20)
+        for letter in range(ord("G"), ord("L")+1):
+            for i in range(1, 21):
+                self.motos.append(f"{P}{chr(letter)}{i}")
+
+        # Para carros (de "B1" a "F16", incrementando de 16 en 16)
+        for letter in range(ord("B"), ord("F")+1):
+            for i in range(1, 17):
+                self.carros.append(f"{P}{chr(letter)}{i}")
+
+        # Para discapacitados (de "A1" a "A10", incrementando de 10 en 10)
+        for i in range(1, 11):
+            self.discapacitados.append(f"{P}A{i}")
+
+        print("Motos:", self.motos)
+        print("Carros:", self.carros)
+        print("Discapacitados:", self.discapacitados)
+
     def addVehicle(self, vehicle):
-        self.plates.append(vehicle.getPlate())
-        self.slots.append(vehicle.getSlot())
-        self.entryTime.append(vehicle.getTime())
-        self.type.append(vehicle.getType())
 
-        # La misma vaina
+      enc = False
 
-        self.vehicles.append(vehicle)
+      for i in self.plates:
+        if vehicle.getPlate() == i:
+          enc = True
+
+      if enc == False:
+        slot_available = False
+        if vehicle.getVType() == "M":  # Para motos
+            for i in self.motos:
+                if i == vehiculo.getSlot():
+                  slot_available = True
+        elif vehicle.getVType() == "C":  # Para carros
+            for i in self.carros:
+                if i == vehiculo.getSlot():
+                  slot_available = True
+        elif vehicle.getVType() == "D":  # Para discapacitados
+            for i in self.discapacitados:
+                if i == vehiculo.getSlot():
+                  slot_available = True
+        if slot_available:
+            if vehicle.getSlot() not in self.slots:
+                self.plates.append(vehicle.getPlate())
+                self.slots.append(vehicle.getSlot())
+                self.entryTime.append(vehicle.getTime())
+                self.VType.append(vehicle.getVType())
+
+                self.vehicles.append(vehicle)
+                print("Vehiculo añadido exitosamente.")
+            else:
+                print("Lugar ocupado. Intentando asignar a otro lugar...")
+                # Aquí podrías implementar la lógica para encontrar una ubicación de estacionamiento alternativa
+        else:
+            print("Lugar no disponible.")
+      else:
+        print("Placa repetida")
+
 
     def findVehicle(self, plate): # Se busca el vehiculo en el piso
         for vehicle in self.vehicles:
@@ -73,22 +99,25 @@ class Piso:
             exit_seconds = int(exit_time[0]) * 3600 + int(exit_time[1]) * 60
             duration = exit_seconds - entry_seconds
 
-            hours_parked = duration / 3600
-            fee = hours_parked * 2000
-            self.removeVehicle(plate)
-            return fee
+            if entry_seconds < exit_seconds:
+              hours_parked = duration / 3600
+              fee = hours_parked * 2000
+              self.removeVehicle(plate)
+              return fee
+            else:
+              return print("Tiempo Invalido")
         else:
-            return None
+            return print("Vehiculo no encontrado")
 
 class Vehicle:
 
     # Tiempo del vehiculo evaluado de 00:00 hasta 23:59
 
-    def __init__(self, plate, slot, time, type):
+    def __init__(self, plate, slot, time, vType):
         self.plate = plate
         self.slot = slot
         self.time = time
-        self.type = type
+        self.vType = vType
 
     def getPlate(self):
         return self.plate
@@ -99,31 +128,29 @@ class Vehicle:
     def getTime(self):
         return self.time
 
-    def getType(self):
-        return self.type
+    def getVType(self):
+        return self.vType
 
 # Creacion de pisos
 
 Piso_1 = Piso("P1")
-Piso_2 = Piso("P2")
-Piso_3 = Piso("P3")
+# Piso_2 = Piso("P2")
+# Piso_3 = Piso("P3")
 
 # Para la creacion de vehiculos, se necesita PLATE, SLOT, TIME Y TYPE
 
 # Tipos: Moto, Carro, Discapacitados
 
 
-vehiculo = Vehicle("ABM650", "P1B1", "10:30", "M")
+vehiculo = Vehicle("ABM650", "P1B1", "10:30", "C")
 Piso_1.addVehicle(vehiculo)
 
-print("Se tiene que pagar: ", Piso_1.clearVehicle("ABM650", "13:30"))
+cogoyocar = Vehicle("ABM650", "P1B2", "10:30", "C")
+Piso_1.addVehicle(cogoyocar)
 
+# Piso_1.clearVehicle("ABM650","9:30")
 
-
-
-
-
-
+juepuchacar = Vehicle("GTZ200", "P1B1", "10:30", "C")
 
 
 
